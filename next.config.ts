bc03c6f2c4ca18a976fd6dec97c62ff08070bb46
jiminu <jmu417@gmail.com>
2025-10-05 21:35:55 +0900

    fix: export to static

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+  trailingSlash: true,
   /* config options here */
 };
 
