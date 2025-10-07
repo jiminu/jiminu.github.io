f62be512c834e5bc8a86702d602e6001e0557310
jiminu <jmu417@gmail.com>
2025-10-07 22:31:14 +0900

    update: card image

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,8 +23,8 @@ export const metadata: Metadata = {
     images: [
       {
         url: '/card_image.png',
-        width: 1200,
-        height: 630,
+        // width: 1200,
+        // height: 630,
         alt: 'preview',
       },
     ],
